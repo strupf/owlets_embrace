58765ef5af4073c7966c9d903e6d4e98b96c160d
Strupf <lukas.wolski@t-online.de>
2023-07-25 14:19:32 +0200

    Test build

---
+++ build_desktop.bat
@@ -0,0 +1,4 @@
+gcc -g -o main src/main.c
+cv2pdb main.exe
+remedybg main.exe
+pause
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+        int a = 4;
+        int b = 5;
+        int c = a + b;
+        printf("Hello world %i\n", c);
+        return c;
+}
